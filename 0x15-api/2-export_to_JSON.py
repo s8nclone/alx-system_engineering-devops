49b0dbe6abf39c2b6389b184b772b61c52066cc9
s8nclone <dayojnr.3@gmail.com>
2023-08-03 19:34:42 -0700

    export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -6,14 +6,14 @@ import sys
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    api = "https://jsonplaceholder.typicode.com/"
+    usr = requests.get(api + "users/{}".format(user_id)).json()
+    usrname = usr.get("username")
+    todos = requests.get(api + "todos", params={"userId": user_id}).json()
 
     with open("{}.json".format(user_id), "w") as jsonfile:
         json.dump({user_id: [{
                 "task": t.get("title"),
                 "completed": t.get("completed"),
-                "username": username
+                "username": usrname
             } for t in todos]}, jsonfile)
