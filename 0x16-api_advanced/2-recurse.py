a69875c8b78a452de30c0a2e791504e9c3e0b54c
s8nclone <dayojnr.3@gmail.com>
2023-08-20 18:56:09 -0700

    recurse it

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -18,7 +18,7 @@ def recurse(subreddit, hot_list=[], after="", count=0):
         "limit": 100
     }
     resp = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+                        allow_redirects=False)
     if resp.status_code == 404:
         return None
 
